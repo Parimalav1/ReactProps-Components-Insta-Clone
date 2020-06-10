4053924c217969b58d599d18a448e4a3180320e0
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-06-09 22:15:09 -0700

    React props

--- src/App.js
+++ src/App.js
@@ -4,18 +4,35 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
+import PostsPage from './components/PostsContainer/PostsPage';
+// import PostHeader from './PostHeader'
+import SearchBar from './components/SearchBar/SearchBarContainer'
+import PostHeader from "./components/PostsContainer/PostHeader";
+// import PostHeader from './PostHeader'
 
-
-const App = () => {
-
+const App = (props) => {
+  // const [postHeaders, setpostHeaders] = useState(dummyData)
+  const [searchText, setSearchText] = useState('');
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostsPage />
+      {/* <Post listOfPostHeaders={postHeaders.filter(x => {
+        if (!searchText || x.name.includes(searchText)) {
+          return PostHeader
+        }
+        return false
+      })} /> */}
     </div>
   );
 };
 
+
 export default App;
+
+
+
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,24 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from '../../dummy-data' 
 
-const PostsPage = () => {
+const PostsPage = (props) => {
+  const [posts, setPosts] = useState(dummyData);
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {/* <Post post={posts[0]}/>
+      <Post post={posts[1]}/> */}
+      {
+        posts.map(x => {
+          return <Post post={x}/>
+        })
+      }
     </div>
   );
 };
